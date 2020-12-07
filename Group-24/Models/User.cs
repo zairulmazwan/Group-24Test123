31f3965b99226576d728ea55341d9a5cc8dc4418
Nate Ainsworth <nathanaelains@gmail.com>
2020-12-07 15:09:11 +0000

    View User list on simple webpage

--- Group-24/Models/User.cs
+++ Group-24/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,22 @@ namespace Group_24.Models
 {
     public class User    {
 
+        [Display(Name = "User ID")]
         public int UserID { get; set; }
 
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
 
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
+        [Display(Name = "Mod Level")]
         public int ModLevel { get; set; }
 
     }
---
+++ Group-24/Pages/Users/View.cshtml.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Group_24.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace Group_24.Pages.Users
+{
+    public class ViewModel : PageModel
+    {
+
+        public List<User> UserRecords { get; set; }
+
+        public IActionResult OnGet()
+        {
+            string G24database_connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=G24Database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection connect = new SqlConnection(G24database_connection);
+            connect.Open();
+
+            using (SqlCommand command = new SqlCommand())
+            {
+
+                command.Connection = connect;
+                //sets all new users to a modlevel of 0
+                command.CommandText = @"SELECT * FROM Users";
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                UserRecords = new List<User>();
+
+                while (reader.Read())
+                {
+                    User record = new User();
+                    record.UserID = reader.GetInt32(0);
+                    record.FirstName = reader.GetString(1);
+                    record.LastName = reader.GetString(1);
+                    record.EmailAddress = reader.GetString(1);
+                    record.Password = reader.GetString(1);
+                    record.ModLevel = reader.GetInt32(0);
+
+
+                    UserRecords.Add(record);
+                }
+                reader.Close();
+
+            }
+
+            return Page();
+        }
+
+         
+    }
+}
