09828293ef85ae8a79cac7a96ebe52e1c360f85a
Nate Ainsworth <nathanaelains@gmail.com>
2020-12-08 16:47:39 +0000

    fixed update user

--- Group-24/Pages/Users/Update.cshtml.cs
+++ Group-24/Pages/Users/Update.cshtml.cs
@@ -15,8 +15,44 @@ namespace Group_24.Pages.Users
         public User UserRecord { get; set; }
 
 
-        public void OnGet()
+        public IActionResult OnGet(int?id)
         {
+            string G24database_connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nate\source\repos\2\Group-24\Group-24\Data\G24Database.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection connect = new SqlConnection(G24database_connection);
+            connect.Open();
+
+            UserRecord = new User();
+
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = connect;
+                //sets all new users to a modlevel of 0
+                command.CommandText = "SELECT * FROM Users WHERE UserID = @ID";
+
+                command.Parameters.AddWithValue("@ID", id);
+
+                Console.WriteLine("The id: " + id);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    UserRecord.UserID = reader.GetInt32(0);
+                    UserRecord.FirstName = reader.GetString(1);
+                    UserRecord.LastName = reader.GetString(2);
+                    UserRecord.EmailAddress = reader.GetString(3);
+                    UserRecord.Password = reader.GetString(4);
+                    UserRecord.ModLevel = reader.GetInt32(5);
+
+                }
+
+
+
+            }
+            connect.Close();
+
+
+            return Page();
         }
 
         public IActionResult OnPost()
@@ -25,31 +61,37 @@ namespace Group_24.Pages.Users
             SqlConnection connect = new SqlConnection(G24database_connection);
             connect.Open();
 
+            Console.WriteLine("User ID" + UserRecord.UserID);
+            Console.WriteLine("User First Name" + UserRecord.FirstName);
+            Console.WriteLine("User Last Name" + UserRecord.LastName);
+            Console.WriteLine("User Email Address" + UserRecord.EmailAddress);
+            Console.WriteLine("User Password" + UserRecord.Password);
+            Console.WriteLine("User Mod Level" + UserRecord.ModLevel);
+
+
             using (SqlCommand command = new SqlCommand())
             {
 
                 command.Connection = connect;
                 //sets all new users to a modlevel of 0
-                command.CommandText = @"INSERT INTO Users (FirstName,LastName,EmailAddress,Password) VALUES ( @FName, @LName, @Email, @Password)";
+                command.CommandText = "UPDATE Users SET UserID = @UID, FirstName = @FName, LastName = @LName, EmailAddress = @Email, Password = @Password, ModLevel = @ModLevel";
 
-
+                command.Parameters.AddWithValue("@UID", UserRecord.UserID);
                 command.Parameters.AddWithValue("@FName", UserRecord.FirstName);
                 command.Parameters.AddWithValue("@LName", UserRecord.LastName);
                 command.Parameters.AddWithValue("@Email", UserRecord.EmailAddress);
                 command.Parameters.AddWithValue("@Password", UserRecord.Password);
-
-                Console.WriteLine(UserRecord.FirstName);
-                Console.WriteLine(UserRecord.LastName);
-                Console.WriteLine(UserRecord.EmailAddress);
-                Console.WriteLine(UserRecord.Password);
+                command.Parameters.AddWithValue("@ModLevel", UserRecord.ModLevel);
 
 
                 command.ExecuteNonQuery();
 
             }
 
+            connect.Close();
 
-            return RedirectToPage("/Index");
+
+            return RedirectToPage("/Users/View");
         }
 
 
